468b22b9c4d741ebe455c4f2d1f5f38bf7225292
lhydave <lhydave@163.com>
2021-05-01 01:14:20 +0800

    update cut_video srcipt

---
+++ src/cut_videos.py
@@ -0,0 +1,16 @@
+# cut videos into 5:00
+# open it in NetLab1
+import os
+from tqdm import tqdm
+total = 40
+def fin_name(x): return "Records/test{}.mp4".format(x)
+def fout_name(x): return "EditedVideo/test{}.mp4".format(x)
+
+
+def cmd_line(
+    x): return "ffmpeg -ss 00:00:00 -t 00:05:00 -i {0} -vcodec copy -acodec copy {1}".format(fin_name(x), fout_name(x))
+
+
+if __name__ == "__main__":
+    for i in tqdm(range(1, total + 1)):
+        os.system(cmd_line(i))
